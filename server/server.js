3303dbcda80686862e17a857e16cfc072345aa9d
Anik Das <anikdas.2113@gmail.com>
2023-03-03 05:07:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 var bodyParser = require('body-parser');
+const path = require('path');
 
 const MONGODB_URL = 'mongodb+srv://admin:admin@portify.fpzijgb.mongodb.net/assignment'
 
